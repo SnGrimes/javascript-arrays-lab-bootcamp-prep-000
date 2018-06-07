e8e5a4cddffd1ec93c2691b15f84595f3aa2d45f
Shantia Grimes-Farmer <shantiagrimes@gmail.com>
2018-06-07 17:17:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ var kittens = ['Milo','Otis', 'Garfield'];
 function destructivelyAppendKitten(name) {
   return kittens.push(name);
 }
+
+function destructivelyPrependKitten(name) {
+  return kittens.unshift(name);
+}
+
+function 
